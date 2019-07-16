4f4f14bee8a361a46b05370b324c9520e52a42f8
mdatsev <martin.datsev.p@gmail.com>
2019-07-16 16:56:13 +0300

    passwords

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = new (require('koa-router'))()
 const db = require('../db.js')
+const bcrypt = require('bcrypt');
 
 router.get('/login', async (ctx, next) => {
     await ctx.render('login')
@@ -7,6 +8,9 @@ router.get('/login', async (ctx, next) => {
 
 router.post('/login', async (ctx, next) => {
     const {email, password, remember} = ctx.request.body
+    const result = await db.query(`SELECT "password" FROM "user" WHERE "email" = $1`, [email])
+    const hash = result.rows[0].password
+    ctx.body = await comparePassword(password, hash)  
 })
 
 router.get('/register', async (ctx, next) => {
@@ -15,6 +19,19 @@ router.get('/register', async (ctx, next) => {
 
 router.post('/register', async (ctx, next) => {
     const {name, email, password} = ctx.request.body
+    const password_hash = await hashPassword(password)
+    await db.query(
+        `INSERT INTO "user" ("name", "email", "password") VALUES ($1, $2, $3)`, 
+        [name, email, password_hash]
+    )
 })
+
+function hashPassword(password) {
+    return bcrypt.hash(password, 10)
+}
+
+function comparePassword(password, hash) {
+    return bcrypt.compare(password, hash)
+}
 
 module.exports = router
