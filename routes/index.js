4aaacf4853bd1ab6078e169679f0cf1fb0cb72a7
mdatsev <martin.datsev.p@gmail.com>
2019-07-19 12:00:15 +0300

    eslint semistandard

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
-const router = new (require('koa-router'))()
-const db = require('../db.js')
+const router = new (require('koa-router'))();
+const db = require('../db.js');
 const bcrypt = require('bcrypt');
 
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login')
-})
+  await ctx.render('login');
+});
 
 router.post('/login', async (ctx, next) => {
-    const {email, password, remember} = ctx.request.body
-    const result = await db.query(`SELECT "password" FROM "user" WHERE "email" = $1`, [email])
-    const hash = result.rows[0].password
-    ctx.body = await comparePassword(password, hash)  
-})
+  const { email, password, remember } = ctx.request.body;
+  const result = await db.query(`SELECT "password" FROM "user" WHERE "email" = $1`, [email]);
+  const hash = result.rows[0].password;
+  ctx.body = await comparePassword(password, hash);
+});
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register')
-})
+  await ctx.render('register');
+});
 
 router.post('/register', async (ctx, next) => {
-    const {name, email, password} = ctx.request.body
-    const password_hash = await hashPassword(password)
-    await db.query(
-        `INSERT INTO "user" ("name", "email", "password") VALUES ($1, $2, $3)`, 
-        [name, email, password_hash]
-    )
-})
+  const { name, email, password } = ctx.request.body;
+  const passwordHash = await hashPassword(password);
+  await db.query(
+    `INSERT INTO "user" ("name", "email", "password") VALUES ($1, $2, $3)`,
+    [name, email, passwordHash]
+  );
+});
 
-function hashPassword(password) {
-    return bcrypt.hash(password, 10)
+function hashPassword (password) {
+  return bcrypt.hash(password, 10);
 }
 
-function comparePassword(password, hash) {
-    return bcrypt.compare(password, hash)
+function comparePassword (password, hash) {
+  return bcrypt.compare(password, hash);
 }
 
-module.exports = router
+module.exports = router;
