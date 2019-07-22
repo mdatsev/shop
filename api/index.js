1433226362597880ed3e570e4e3007119bb816a9
mdatsev <martin.datsev.p@gmail.com>
2019-07-22 19:00:47 +0300

    some error handling

--- api/index.js
+++ api/index.js
@@ -1,15 +1,47 @@
 const methods = require('./methods.js');
 
-module.exports = async (ctx, next) => {
-  const apiCtx = {
-    organizationId: 1,
-  };
-  const { method, params, id } = ctx.request.body;
-  const result = await methods[method](params, apiCtx);
+// const PARSE_ERROR = -32700;
+// const INVALID_REQUEST = -32600;
+const METHOD_NOT_FOUND = -32601;
+// const INVALID_PARAMS = -32602;
+// const INTERNAL_ERROR = -32603;
+// const SERVER_ERROR = -32000 to -32099;
 
-  ctx.body = {
-    jsonrpc: '2.0',
-    result,
-    id,
-  };
+module.exports = async (ctx, next) => {
+  const { method, params, id } = ctx.request.body;
+
+  function success (result) {
+    ctx.body = {
+      jsonrpc: '2.0',
+      result,
+      id,
+    };
+  }
+
+  function error (code, message, data) {
+    ctx.body = {
+      jsonrpc: '2.0',
+      error: {
+        code,
+        message,
+        data,
+      },
+      id,
+    };
+  }
+
+  try {
+    const apiCtx = {
+      organizationId: 1,
+    };
+
+    if (method in methods) {
+      success(await methods[method](params, apiCtx));
+    } else {
+      error(METHOD_NOT_FOUND, `Method not found.`, { method });
+    }
+  } catch (e) {
+    error(-1, 'Uncaught exception', { debug: e });
+    throw e;
+  }
 };
