2fda417d9dfa195347be5f488d5a5ee51b980324
mdatsev <martin.datsev.p@gmail.com>
2019-07-23 12:30:38 +0300

    simplified code and removed some deps

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
 const path = require('path');
-
 const Koa = require('koa');
-const app = new Koa();
-const views = require('koa-views');
-const json = require('koa-json');
-const onerror = require('koa-onerror');
-const bodyparser = require('koa-bodyparser');
 const logger = require('koa-logger');
+const bodyparser = require('koa-bodyparser');
+const views = require('koa-views');
 
 const index = require('./routes/index');
 const api = require('./routes/api');
 
-// error handler
-onerror(app);
+const app = new Koa();
 
-// middlewares
+app.use(logger());
 app.use(bodyparser({
   enableTypes: ['json', 'form', 'text'],
 }));
-app.use(json());
-app.use(logger());
-app.use(require('koa-static')(path.join(__dirname, 'public')));
 
+app.use(api.routes(), api.allowedMethods());
+
+app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(views(path.join(__dirname, 'views'), {
   extension: 'pug',
 }));
 
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date();
-
-  await next();
-  const ms = new Date() - start;
-
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
-
-// routes
 app.use(index.routes(), index.allowedMethods());
-app.use(api.routes(), api.allowedMethods());
 
-// error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx);
+  console.error(err);
 });
 
 module.exports = app;
