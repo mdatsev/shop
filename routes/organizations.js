01c45bc370722347c4080ce5789e35858eeb987f
mdatsev <martin.datsev.p@gmail.com>
2019-07-25 17:35:12 +0300

    edit products and subscrs views

--- routes/organizations.js
+++ routes/organizations.js
@@ -3,6 +3,7 @@ const { loggedIn } = require('../middleware/user_auth.js');
 
 const organization = require('../dbmodels/organization.js');
 const product = require('../dbmodels/product.js');
+const subscription = require('../dbmodels/subscription.js');
 
 router.get('/', loggedIn, async ctx =>
   ctx.render('orgs/index', {
@@ -14,8 +15,9 @@ router.get('/create', loggedIn, ctx => ctx.render('orgs/create'));
 router.post('/create', loggedIn, async ctx => {
   const { name } = ctx.request.body;
 
-  await organization.create({ name, ownerId: ctx.auth.userId });
-  ctx.redirect('/orgs');
+  const id = await organization.create({ name, ownerId: ctx.auth.userId });
+
+  ctx.redirect(`/orgs/${id}/edit/index`);
 });
 
 router.get('/:id/edit/index', loggedIn, async ctx => {
@@ -27,7 +29,59 @@ router.post('/:id/edit/index', loggedIn, async ctx => {
 });
 
 router.get('/:id/edit/products', loggedIn, async ctx => {
-  await ctx.render('orgs/edit/products', { products: await product.getAllOrg(ctx.params.id) });
+  await ctx.render('orgs/edit/products/index', { products: await product.getAllOrg(ctx.params.id) });
+});
+
+router.get('/:id/edit/products/create', loggedIn, async ctx => {
+  await ctx.render('orgs/edit/products/create', { products: await product.getAllOrg(ctx.params.id) });
+});
+
+router.post('/:id/edit/products/create', loggedIn, async ctx => {
+  const { name, description, price, availableQuantity } = ctx.request.body;
+
+  await product.create({
+    name,
+    description,
+    price,
+    availableQuantity,
+    organizationId: ctx.params.id,
+  });
+
+  ctx.redirect('.');
+});
+
+router.get('/:orgId/edit/products/:productId', loggedIn, async ctx => {
+  await ctx.render('orgs/edit/products/edit', await product.get(ctx.params.productId));
+});
+
+router.get('/:id/edit/subscriptions', loggedIn, async ctx => {
+  await ctx.render('orgs/edit/subscriptions/index', { subscriptions: await subscription.getAllOrg(ctx.params.id) });
+});
+
+router.get('/:id/edit/subscriptions/create', loggedIn, async ctx => {
+  await ctx.render('orgs/edit/subscriptions/create', { subscriptions: await subscription.getAllOrg(ctx.params.id) });
+});
+
+router.post('/:id/edit/subscriptions/create', loggedIn, async ctx => {
+  const { name, description, price, period } = ctx.request.body;
+
+  await subscription.create({
+    name,
+    description,
+    price,
+    period,
+    organizationId: ctx.params.id,
+  });
+
+  ctx.redirect('.');
+});
+
+router.get('/:orgId/edit/subscriptions/:subscriptionId', loggedIn, async ctx => {
+  const subscr = await subscription.get(ctx.params.subscriptionId);
+
+  subscr.period = Object.entries(subscr.period).map(([duration, number]) => `${number} ${duration}`).join(' ');
+
+  await ctx.render('orgs/edit/subscriptions/edit', subscr);
 });
 
 module.exports = router;
