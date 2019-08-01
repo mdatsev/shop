a615ba5eb1150c653b75454078252118cd43e34d
mdatsev <martin.datsev.p@gmail.com>
2019-08-01 14:41:37 +0300

    added assert types

--- util/assert.js
+++ util/assert.js
@@ -1,3 +1,17 @@
-const assert = require('assert').strict;
+class BaseAssertionError extends Error { }
 
-module.exports = assert;
+class SystemError extends BaseAssertionError { }
+class UserError extends BaseAssertionError { }
+class PeerError extends BaseAssertionError { }
+
+module.exports = {
+  system: (condition, message) => {
+    if (!condition) throw new SystemError(message);
+  },
+  user: (condition, message) => {
+    if (!condition) throw new UserError(message);
+  },
+  peer: (condition, message) => {
+    if (!condition) throw new PeerError(message);
+  },
+};
