783b44d11c6ccbd5f5496ef1dbfceec0980b1df7
mdatsev <martin.datsev.p@gmail.com>
2019-08-06 11:06:32 +0300

    added api params schemas

--- api/methods.js
+++ api/methods.js
@@ -2,15 +2,20 @@ const product = require('../dbmodels/product.js');
 const subscription = require('../dbmodels/subscription.js');
 
 const api = {
-  async createProduct ({ name, price, description, specs, availableQuantity }, apiCtx) {
+  async createProduct ({ name, price, description, specs, image, availableQuantity }, apiCtx) {
     return product.create({
       name,
       description,
       price,
       specs,
       availableQuantity,
+      image,
       organizationId: apiCtx.organizationId,
     });
+  },
+
+  async getProduct ({ id }, apiCtx) {
+    return product.get(id);
   },
 
   async updateProduct ({ id, name, price, description, specs, availableQuantity }, apiCtx) {
@@ -25,54 +30,51 @@ const api = {
     return {};
   },
 
-  // async getProduct ({ id }, apiCtx) {
-  //   return product.get(id);
-  // },
+  async deleteProduct ({ id }, apiCtx) {
+    await product.delete(id);
+    return {};
+  },
 
-  // async deleteProduct ({ id }, apiCtx) {
-  //   await product.delete(id);
-  //   return {};
-  // },
+  async getAllOrgProducts (_params, apiCtx) {
+    return product.getAllOrg(apiCtx.organizationId);
+  },
 
-  // async getAllOrgProducts (_params, apiCtx) {
-  //   return product.getAllOrg(apiCtx.organizationId);
-  // },
+  async createSubscription ({ name, description, price, image, specs, period }, apiCtx) {
+    return subscription.create({
+      name,
+      description,
+      price,
+      image,
+      specs,
+      period,
+      organizationId: apiCtx.organizationId,
+    });
+  },
 
-  // async createSubscription ({ name, description, price, specs, period }, apiCtx) {
-  //   return subscription.create({
-  //     name,
-  //     description,
-  //     price,
-  //     specs,
-  //     period,
-  //     organizationId: apiCtx.organizationId,
-  //   });
-  // },
+  async getSubscription ({ id }, apiCtx) {
+    return subscription.get(id);
+  },
 
-  // async updateSubscription ({ id, name, price, description, specs, period }, apiCtx) {
-  //   await subscription.update({
-  //     id,
-  //     name,
-  //     description,
-  //     price,
-  //     specs,
-  //     period,
-  //   });
-  //   return {};
-  // },
+  async updateSubscription ({ id, name, price, description, specs, period }, apiCtx) {
+    await subscription.update({
+      id,
+      name,
+      description,
+      price,
+      specs,
+      period,
+    });
+    return {};
+  },
 
-  // async getSubscription ({ id }, apiCtx) {
-  //   return subscription.get(id);
-  // },
+  async deleteSubscription ({ id }, apiCtx) {
+    await subscription.delete(id);
+    return {};
+  },
 
-  // async deleteSubscription ({ id }, apiCtx) {
-  //   await subscription.delete(id);
-  //   return {};
-  // },
-
-  // async getAllOrgSubscriptions (_params, apiCtx) {
-  //   return subscription.getAllOrg(apiCtx.organizationId);
-  // },
+  async getAllOrgSubscriptions (_params, apiCtx) {
+    return subscription.getAllOrg(apiCtx.organizationId);
+  },
 };
 
 module.exports = api;
